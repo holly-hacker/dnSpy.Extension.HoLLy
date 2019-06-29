f900dd38c76195617ebf473f3b3f22cbb08e5f5e
HoLLy <dario.db@hotmail.com>
2019-06-30 01:22:09 +0200

    Move debug stuff to its own top-level namespace

--- dnSpy.Extension.HoLLy/Constants.cs
+++ dnSpy.Extension.HoLLy/Constants.cs
@@ -2,6 +2,6 @@ namespace HoLLy.dnSpy.Extension
 {
     public static class Constants
     {
-        public const string ContextMenuGroupDebug = "-999,HoLLy.Utilities.Debug";
+        public const string ContextMenuGroupDebug = "-999,HoLLy.ContextMenu.Debug";
     }
 }
