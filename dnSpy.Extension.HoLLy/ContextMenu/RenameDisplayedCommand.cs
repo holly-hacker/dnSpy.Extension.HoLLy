8c119389dcde3c34ba754cb5947c97195f16fe67
HoLLy <dario.db@hotmail.com>
2019-07-01 18:28:38 +0200

    Fix changed names not appearing due to cache

--- dnSpy.Extension.HoLLy/ContextMenu/RenameDisplayedCommand.cs
+++ dnSpy.Extension.HoLLy/ContextMenu/RenameDisplayedCommand.cs
@@ -1,12 +1,7 @@
-using System.Collections.Generic;
-using System.Linq;
 using dnlib.DotNet;
 using dnSpy.Contracts.App;
-using dnSpy.Contracts.Decompiler;
-using dnSpy.Contracts.Documents.Tabs;
 using dnSpy.Contracts.Documents.Tabs.DocViewer;
 using dnSpy.Contracts.Menus;
-using HoLLy.dnSpy.Extension.Decompilers.Decorators;
 
 namespace HoLLy.dnSpy.Extension.ContextMenu
 {
@@ -23,21 +18,8 @@ namespace HoLLy.dnSpy.Extension.ContextMenu
             if (!string.IsNullOrEmpty(newName))
                 TempRenameCache.SetName(m, newName);
 
-            // BUG: this is not enough to re-decompile, there must be some cache
             var documentTabService = docViewer.DocumentTab.DocumentTabService;
-            RefreshTabs(documentTabService);
-        }
-
-        private static void RefreshTabs(IDocumentTabService documentTabService)
-        {
-            IEnumerable<(IDocumentTab tab, IDecompiler decompiler)> decompilerTabs = documentTabService.VisibleFirstTabs.Select(tab => new {
-                    tab,
-                    decompiler = (tab.Content as IDecompilerTabContent)?.Decompiler
-                })
-                .Where(t => !(t.decompiler is null))
-                .Select(t => (t.tab, t.decompiler));
-
-            documentTabService.Refresh(decompilerTabs.Where(t => t.decompiler is DecompilerDecorator).Select(a => a.tab).ToArray());
+            documentTabService.RefreshModifiedDocument(documentTabService.DocumentTreeView.FindNode(m.Module).Document);
         }
 
         public override bool IsVisible(IMenuItemContext context)
