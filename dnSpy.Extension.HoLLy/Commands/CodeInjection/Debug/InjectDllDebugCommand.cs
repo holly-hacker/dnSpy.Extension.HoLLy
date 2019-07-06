36a5f2fc3cd3d307708ce4997243fd18b2c7d932
HoLLy <dario.db@hotmail.com>
2019-07-07 01:33:55 +0200

    Allow exiting early in dll injection debug command

--- dnSpy.Extension.HoLLy/Commands/CodeInjection/Debug/InjectDllDebugCommand.cs
+++ dnSpy.Extension.HoLLy/Commands/CodeInjection/Debug/InjectDllDebugCommand.cs
@@ -17,13 +17,16 @@ namespace HoLLy.dnSpyExtension.Commands.CodeInjection.Debug
 
         public override void Execute(IMenuItemContext context)
         {
-            var pid = MsgBox.Instance.Ask<int>("Process ID");
-            var x86 = MsgBox.Instance.Ask<bool>("Is x86?");
+            var pid = MsgBox.Instance.Ask<int?>("Process ID");
+            if (pid is null) return;
+
+            var x86 = MsgBox.Instance.Ask<bool?>("Is x86?");
+            if (x86 is null) return;
 
             if (!InjectDllCommand.AskForEntryPoint(out MethodDef method, out string parameter))
                 return;
 
-            injector.Inject(pid, method, parameter, x86, RuntimeType.FrameworkV4);
+            injector.Inject(pid.Value, method, parameter, x86.Value, RuntimeType.FrameworkV4);
         }
 
         public override bool IsVisible(IMenuItemContext context) => Utils.IsDebugBuild;
