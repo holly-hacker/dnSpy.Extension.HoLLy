a1510a294ef2b3c1e682eab02972ba0c9cc03921
HoLLy <dario.db@hotmail.com>
2019-07-18 22:56:31 +0200

    Clean up UnityInjector

--- dnSpy.Extension.HoLLy/CodeInjection/Injectors/UnityInjector.cs
+++ dnSpy.Extension.HoLLy/CodeInjection/Injectors/UnityInjector.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using dnSpy.Contracts.App;
 using HoLLy.dnSpyExtension.Common.CodeInjection;
 using Iced.Intel;
 
@@ -30,18 +28,23 @@ namespace HoLLy.dnSpyExtension.CodeInjection.Injectors
 
             // Call remote functions to do actual injection
             const int amountOfParameters = 1;
-            var rootDomain = call("mono_get_root_domain");
+            (IntPtr, IntPtr)? rootDomainInfo = null;
+            IntPtr rootDomain = call("mono_get_root_domain");
             var rawImage = call("mono_image_open", allocCString(args.Path), IntPtr.Zero);
-            var assembly = callThread("mono_assembly_load_from_full", rawImage, allocCString(""), IntPtr.Zero, 0);
-            var image = callThread("mono_assembly_get_image", assembly);
+
+            // from now on, run mono_thread_attach every time
+            rootDomainInfo = (getFunction("mono_thread_attach"), rootDomain);
+
+            var assembly = call("mono_assembly_load_from_full", rawImage, allocCString(""), IntPtr.Zero, 0);
+            var image = call("mono_assembly_get_image", assembly);
             // TODO: what if null namespace?
-            var @class = callThread("mono_class_from_name", image, allocCString(args.Namespace), allocCString(args.Type));
-            var method = callThread("mono_class_get_method_from_name", @class, allocCString(args.Method), amountOfParameters);
+            var @class = call("mono_class_from_name", image, allocCString(args.Namespace), allocCString(args.Type));
+            var method = call("mono_class_get_method_from_name", @class, allocCString(args.Method), amountOfParameters);
 
             // allocate arguments
             var pArg1 = call("mono_string_new", rootDomain, allocCString(args.Argument));
             var pArgs = allocBytes(x86 ? BitConverter.GetBytes(pArg1.ToInt32()) : BitConverter.GetBytes(pArg1.ToInt64()));
-            callThreadNoWait("mono_runtime_invoke", method, IntPtr.Zero, pArgs, IntPtr.Zero);
+            callNoWait("mono_runtime_invoke", method, IntPtr.Zero, pArgs, IntPtr.Zero);
 
             return;
 
@@ -49,47 +52,37 @@ namespace HoLLy.dnSpyExtension.CodeInjection.Injectors
                 ? module.BaseAddress + exports[fun]
                 : throw new Exception($"Could not find exported function {fun} in {module.ModuleName}");
 
-            IntPtr call(string method, params object[] arguments)
+            IntPtr call(string m, params object[] arguments)
             {
-                var ret = CallFunction(hProc, getFunction(method), arguments, x86);
-                Log($"{method} returned 0x{ret.ToInt64():X}");
+                var ret = CallFunction(hProc, getFunction(m), arguments, x86, rootDomainInfo);
+                Log($"{m} returned 0x{ret.ToInt64():X}");
                 return ret;
             }
 
-            IntPtr callThread(string method, params object[] arguments)
+            void callNoWait(string m, params object[] arguments)
             {
-                var ret = CallFunction(hProc, getFunction(method), arguments, x86, (getFunction("mono_thread_attach"), rootDomain));
-                Log($"{method} returned 0x{ret.ToInt64():X}");
-                return ret;
+                CallFunction(hProc, getFunction(m), arguments, x86, (getFunction("mono_thread_attach"), rootDomain), false);
+                Log($"Executed {m}");
             }
-
-            void callThreadNoWait(string method, params object[] arguments)
-            {
-                CallFunctionNoWait(hProc, getFunction(method), arguments, x86, (getFunction("mono_thread_attach"), rootDomain));
-                Log($"Executed {method}");
-            }
-
-            IntPtr alloc(int size, int protection = 0x04) => Native.VirtualAllocEx(hProc, IntPtr.Zero, (uint)size, 0x1000, protection);
-            void writeBytes(IntPtr address, byte[] b) => Native.WriteProcessMemory(hProc, address, b, (uint)b.Length, out _);
 
             IntPtr allocCString(string? str) => str is null ? IntPtr.Zero : allocBytes(new ASCIIEncoding().GetBytes(str + "\0"));
-            IntPtr allocString(string? str) => str is null ? IntPtr.Zero : allocBytes(new UnicodeEncoding().GetBytes(str + "\0"));
 
             IntPtr allocBytes(byte[] buffer)
             {
-                IntPtr pBuffer = alloc(buffer.Length);
-                writeBytes(pBuffer, buffer);
+                IntPtr pBuffer = Native.VirtualAllocEx(hProc, IntPtr.Zero, (uint)buffer.Length, 0x1000, 0x04);
+                if (buffer.Any(b => b != 0))
+                    Native.WriteProcessMemory(hProc, pBuffer, buffer, (uint)buffer.Length, out _);
                 return pBuffer;
             }
         }
 
-        private IntPtr CallFunction(IntPtr hProc, IntPtr fnAddr, object[] arguments, bool x86, (IntPtr fun, IntPtr addr)? rootDomainInfo = null)
+        private static IntPtr CallFunction(IntPtr hProc, IntPtr fnAddr, object[] arguments, bool x86, (IntPtr fun, IntPtr addr)? rootDomainInfo = null, bool wait = true)
         {
             IntPtr pReturnValue = alloc(IntPtr.Size);
 
             var instructions = new InstructionList();
 
-            // cdecl calling conventions, so we need to clean the stack
+            // mono uses the cdecl calling convention, so clean the stack (x86 only)
             void addCall(IntPtr fn, object[] callArgs) => CodeInjectionUtils.AddCallStub(instructions, fn, callArgs, x86, x86);
 
             if (x86) {
@@ -98,14 +91,15 @@ namespace HoLLy.dnSpyExtension.CodeInjection.Injectors
 
                 addCall(fnAddr, arguments);
                 instructions.Add(Instruction.Create(Code.Mov_rm32_r32, new MemoryOperand(Register.None, pReturnValue.ToInt32()), Register.EAX));
-
                 instructions.Add(Instruction.Create(Code.Retnd));
             } else {
                 throw new NotImplementedException();
             }
 
-            // Log("Instructions to be injected:\n" + string.Join("\n", instructions));
-            CodeInjectionUtils.RunRemoteCode(hProc, instructions, x86, true);
+            CodeInjectionUtils.RunRemoteCode(hProc, instructions, x86, wait);
+
+            if (!wait)
+                return IntPtr.Zero;
 
             var outBuffer = new byte[IntPtr.Size];
             Native.ReadProcessMemory(hProc, pReturnValue, outBuffer, outBuffer.Length, out _);
@@ -113,32 +107,6 @@ namespace HoLLy.dnSpyExtension.CodeInjection.Injectors
             return new IntPtr(IntPtr.Size == 8 ? BitConverter.ToInt64(outBuffer, 0) : BitConverter.ToInt32(outBuffer, 0));
 
             IntPtr alloc(int size, int protection = 0x04) => Native.VirtualAllocEx(hProc, IntPtr.Zero, (uint)size, 0x1000, protection);
-        }
-
-        // TODO: duplicated method is very, very ugly! I want to refactor most of this too
-        private void CallFunctionNoWait(IntPtr hProc, IntPtr fnAddr, object[] arguments, bool x86, (IntPtr fun, IntPtr addr)? rootDomainInfo = null)
-        {
-            var instructions = new InstructionList();
-
-            // cdecl calling conventions, so we need to clean the stack
-            void addCall(Register r, object[] callArgs) => CodeInjectionUtils.AddCallStub(instructions, r, callArgs, x86, x86);
-
-            if (x86) {
-                if (rootDomainInfo.HasValue) {
-                    instructions.Add(Instruction.Create(Code.Mov_r32_imm32, Register.EAX, rootDomainInfo.Value.fun.ToInt32()));
-                    addCall(Register.EAX, new object[] { rootDomainInfo.Value.addr });
-                }
-
-                instructions.Add(Instruction.Create(Code.Mov_r32_imm32, Register.EAX, fnAddr.ToInt32()));
-                addCall(Register.EAX, arguments);
-
-                instructions.Add(Instruction.Create(Code.Retnd));
-            } else {
-                throw new NotImplementedException();
-            }
-
-            // Log("Instructions to be injected:\n" + string.Join("\n", instructions));
-            CodeInjectionUtils.RunRemoteCode(hProc, instructions, x86);
         }
     }
 }
