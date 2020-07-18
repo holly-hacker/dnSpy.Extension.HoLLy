5b390a9b5d77422f4188bc51358e8e4721d9be99
HoLLy <dario.db@hotmail.com>
2020-07-18 23:34:39 +0200

    Only show in debug builds

--- dnSpy.Extension.HoLLy/Common/Commands/Debug/ShowTreeNodeType.cs
+++ dnSpy.Extension.HoLLy/Common/Commands/Debug/ShowTreeNodeType.cs
@@ -8,6 +8,8 @@ namespace HoLLy.dnSpyExtension.Common.Commands.Debug
     [ExportMenuItem(Header = "DBG: View tree node type", Group = Constants.ContextMenuGroupDebug)]
     public class ShowTreeNodeType : SingleTreeViewMenuItemBase
     {
+        public override bool IsVisible(TreeNodeData context) => Utils.IsDebugBuild;
+
         public override string? GetHeader(TreeNodeData context) => $"DBG: {context.GetType().Name} info";
 
         public override void Execute(TreeNodeData context)
