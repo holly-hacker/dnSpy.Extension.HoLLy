2ce7764b3e30a4cb118aec1a20d1a57f9a6de8fc
HoLLy <dario.db@hotmail.com>
2020-10-19 22:37:11 +0200

    Remove tooltips over CFG nodes (closes #41) This uses an ugly reflection hack, but there is no built in way to assign a different tooltip (it is hardcoded to the content of the label). See: https://github.com/microsoft/automatic-graph-layout/blob/b9d18b0ce6d00525068590b8d92667efe1611af6/GraphLayout/tools/WpfGraphControl/VNode.cs#L251

--- dnSpy.Extension.HoLLy/ControlFlowGraph/GraphControl.xaml.cs
+++ dnSpy.Extension.HoLLy/ControlFlowGraph/GraphControl.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 using dnSpy.Contracts.Settings.Fonts;
 using dnSpy.Contracts.Themes;
 using Microsoft.Msagl.WpfGraphControl;
@@ -20,8 +24,47 @@ namespace HoLLy.dnSpyExtension.ControlFlowGraph
             var viewer = new GraphViewer();
             viewer.BindToPanel(Panel);
             viewer.Graph = echoGraph.ToMicrosoftGraph(theme, font);
-
+            SetTooltips(viewer);
             viewer.NodeToCenterWithScale(viewer.Graph.Nodes.OrderBy(n => n.UserData is long u ? u : long.MaxValue).First(), 1);
+        }
+
+        private static void SetTooltips(GraphViewer viewer)
+        {
+            try
+            {
+                // this is very ugly, but I don't know a better way
+                const string labelFieldName = "FrameworkElementOfNodeForLabel";
+                const string pathFieldName = "BoundaryPath";
+                var labelField = typeof(VNode).GetField(labelFieldName, BindingFlags.NonPublic | BindingFlags.Instance)
+                                 ?? throw new Exception($"Couldn't find {nameof(VNode)} field {labelFieldName}");
+                var pathField = typeof(VNode).GetField(pathFieldName, BindingFlags.NonPublic | BindingFlags.Instance)
+                                ?? throw new Exception($"Couldn't find {nameof(VNode)} field {pathFieldName}");
+
+                foreach (var viewerEntity in viewer.Entities)
+                {
+                    if (!(viewerEntity is VNode vNode)) continue;
+
+                    (string? labelTooltip, string? pathTooltip) = GetTooltipsForNode(vNode);
+
+                    var label = (FrameworkElement?) labelField.GetValue(vNode)
+                                ?? throw new Exception($"Found {nameof(vNode)} with null {nameof(labelFieldName)}");
+                    label.ToolTip = labelTooltip;
+
+                    var path = (Path?) pathField.GetValue(vNode)
+                               ?? throw new Exception($"Found {nameof(vNode)} with null {nameof(pathFieldName)}");
+                    path.ToolTip = pathTooltip;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to set graph control tooltips: " + e.Message);
+            }
+        }
+
+        private static (string? labelTooltip, string? pathTooltip) GetTooltipsForNode(VNode _)
+        {
+            // not sure yet what to put as tooltip
+            return (null, null);
         }
     }
 }
