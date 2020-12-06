9ad2f57fea64fb79662690161fc43d2c855bff3c
HoLLy <dario.db@hotmail.com>
2020-12-06 22:00:52 +0100

    Show DIE nodes for PE documents

--- dnSpy.Extension.HoLLy/DetectItEasy/DieTreeNodeDataProvider.cs
+++ dnSpy.Extension.HoLLy/DetectItEasy/DieTreeNodeDataProvider.cs
@@ -5,7 +5,21 @@ using dnSpy.Contracts.TreeView;
 namespace HoLLy.dnSpyExtension.DetectItEasy
 {
     [ExportTreeNodeDataProvider(Guid = DocumentTreeViewConstants.ASSEMBLY_NODE_GUID)]
-    public class DieTreeNodeDataProvider : ITreeNodeDataProvider
+    public class DieAssemblyTreeNodeDataProvider : ITreeNodeDataProvider
+    {
+        public IEnumerable<TreeNodeData> Create(TreeNodeDataProviderContext context)
+        {
+            var data = (DsDocumentNode) context.Owner.Data;
+
+            var peImage = data.Document.PEImage;
+
+            if (peImage?.Filename != null)
+                yield return new DieTreeNode(peImage.Filename);
+        }
+    }
+
+    [ExportTreeNodeDataProvider(Guid = DocumentTreeViewConstants.PEDOCUMENT_NODE_GUID)]
+    public class DiePeTreeNodeDataProvider : ITreeNodeDataProvider
     {
         public IEnumerable<TreeNodeData> Create(TreeNodeDataProviderContext context)
         {
